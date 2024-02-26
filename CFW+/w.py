ce44803ad390b987104babda354e6a489e7020ef
FE <156041767+FarhadElahi@users.noreply.github.com>
2024-02-27 03:16:59 +0330

    Update w.py

--- CFW+/w.py
+++ CFW+/w.py
@@ -8,9 +8,11 @@ import os
 import sys
 os.system("title CloudFlare WARP+")
 os.system('cls' if os.name == 'nt' else 'clear')
-print('-----------------------------------------------------\n')
-print("       CloudFlare WARP+")
-print ("----------------------------------------------------")
+print('--------------------\n')
+print ('-------------------\n')
+print ("-----------------")
+print("CloudFlare WARP+")
+print ("-----------------")
 no = int(input("Type GB Number : "))
 referrer = input("Enter The WARP+ ID : ")
 def genString(stringLength):
