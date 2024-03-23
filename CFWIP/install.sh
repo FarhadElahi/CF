bc25a6ef4f6413be6772210a83ec61bf994fcb3e
FE <156041767+FarhadElahi@users.noreply.github.com>
2024-03-23 22:02:17 +0330

    Update install.sh

--- CFWIP/install.sh
+++ CFWIP/install.sh
@@ -206,13 +206,14 @@ exit
 
 clear
 echo "--------------------------------------------"
-echo -e "${yellow}             CloudFlare WARP IP${rest}"
+echo -e "${orange}          CloudFlare WARP${rest}"
+echo -e "${yellow}                       EndIP${rest}"
 echo "--------------------------------------------"
 echo""
-echo -e "${purple}1.${green}IPV4 preferred peer IP${rest}"
-echo -e "${purple}2.${green}IPV6 preferred peer IP${rest}"
+echo -e "${purple}1.${green}IPV4${rest}"
+echo -e "${purple}2.${green}IPV6${rest}"
 echo -e "${purple}0.${green}Exit${rest}"
-read -p "please choose: " menu
+read -p "Enter: " menu
 if [ "$menu" == "1" ];then
 cfwarpIP && endipv4 && endipresult && Endip_v4
 elif [ "$menu" == "2" ];then
