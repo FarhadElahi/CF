9c2915a20776ed9a47f3368383bb43e7caae83b1
FE <156041767+FarhadElahi@users.noreply.github.com>
2024-03-25 16:32:37 +0330

    Update wa.py

--- CFW+/wa.py
+++ CFW+/wa.py
@@ -21,8 +21,9 @@ for file_path in file_paths:
 else:
     default_id = "Not set"
     
-print("========== CloudFlare WARP+ =========")
-print("╰─━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╯")
+print("╭─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╮⁠")
+print("           CloudFlare WARP+
+print("╰─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╯")
 print("")
 referrer = input(f"[#] Enter Your ID                                     [Your ID: {default_id} ]:") or default_id
 
