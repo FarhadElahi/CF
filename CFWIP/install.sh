297ea69d65a4d8106baa1ff2180a198823ef2e0c
FE <156041767+FarhadElahi@users.noreply.github.com>
2024-03-26 00:10:31 +0330

    Update install.sh

--- CFWIP/install.sh
+++ CFWIP/install.sh
@@ -211,17 +211,17 @@ echo -e "${yellow}                         EndIP"
 echo -e "${yellow}   ╰─━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━─╯"
 echo""
 echo -e "  ${yellow}  ╭─━━━━─╮⁠"
-echo -e "  ${yellow}1.   IPV4   "
+echo -e "  ${yellow}1.  IPV4   "
 echo -e "  ${yellow}  ╰─━━━━─╯"
 echo -e "  ${yellow}  ╭─━━━━─╮⁠"
-echo -e "  ${yellow}2.   IPV6   "
+echo -e "  ${yellow}2.  IPV6   "
 echo -e "  ${yellow}  ╰─━━━━─╯"
 echo -e "  ${yellow}  ╭─━━━━─╮⁠"
-echo -e "  ${yellow}3.   Exit   "
+echo -e "  ${yellow}3.  Exit   "
 echo -e "  ${yellow}  ╰─━━━━─╯"
-echo -e╭─────╮⁠
+echo -e"╭─────╮⁠"
 read -p "Enter:" menu
-echo -e╰─────╯
+echo -e"╰─────╯"
 if [ "$menu" == "1" ];then
 cfwarpIP && endipv4 && endipresult && Endip_v4
 elif [ "$menu" == "2" ];then
