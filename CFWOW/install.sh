a5b3a1aed4a7711a2fbaa38ec7901f5ce25db569
FE <156041767+FarhadElahi@users.noreply.github.com>
2024-03-27 15:36:52 +0330

    Update install.sh

--- CFWOW/install.sh
+++ CFWOW/install.sh
@@ -257,11 +257,11 @@ full_json='
 echo "$full_json" > output.json
 echo ""
 echo "${GREEN}Upload Files to Get Link${RESET}"
-echo "     ━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━⁠"
 echo ""
-echo "Your link:                         ━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━"
+echo "Your link:"
 curl https://bashupload.com/ -T output.json | sed -e 's#wget#Your Link#' -e 's#https://bashupload.com/\(.*\)#https://bashupload.com/\1?download=1#'
-echo "     ━─━─━─━─━─━─━─━─━─━─━─━─━─━─━─━━─━─━─━─━─━─"
+echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 echo ""
 mv output.json output_$(date +"%Y%m%d_%H%M%S").json
 
